fd8598bccaa1b13ca4320b1bff5de6b3ffeac245
Carlos Sivira <carlos.csivira@gmail.com>
2023-02-09 20:26:49 -0400

    (fix) requests

--- test/test_user.py
+++ test/test_user.py
@@ -6,17 +6,17 @@ from user.model import User
 class UserTest(unittest.TestCase):
     BASE_URL = 'http://localhost:5000'
     USER_URL = '{}/user'.format(BASE_URL)
-    USER_ADMIN_OBJ = [
-        'username' = 'example',
-        'email' = 'example@example.com',
-        'password' = 'example',
-        'admin' = 'on'
-    ]
+    USER_ADMIN_OBJ = {
+        'username': 'example',
+        'email': 'example@example.com',
+        'password': 'example',
+        'admin': 'on'
+    }
 
     def test_should_retrieve_all_users(self):
         r = requests.get(self.USER_URL)
         self.assertEqual(r.status_code, 200)
 
-    """ def test_should_register_a_new_user(self):
+    def test_should_register_a_new_user(self):
         r = requests.post(self.USER_URL + '/register', data=self.USER_ADMIN_OBJ)
-        self.assertEqual(r.status_code, 200) """
+        self.assertEqual(r.status_code, 200)
